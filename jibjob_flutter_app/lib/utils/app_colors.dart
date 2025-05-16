711114b9d146e876e2cfbf83def064721e723014
LAGGOUNE Omar <dogie4bogie@gmail.com>
2025-05-16 06:26:07 +0100

    Organisation: adding app_colors.dart & text_styles.dart

---
+++ jibjob_flutter_app/lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color primaryAppColor = Color(0xFF130160); // Your main dark blue/purple
+const Color accentColor = Color(0xFF6D42F4);    // The vibrant purple for "Dream Job"
+const Color darkPurple = Color(0xFF20004E);     // Another purple you use
+const Color lightPurple = Color(0xFF9F58FF);    // Used in JibJobAuthPage
+const Color textPrimaryColor = Color(0xFF130160);
+const Color textSecondaryColor = Color(0xFF524B6B); // Like "Explorez tous les postes..."
+const Color buttonTextColor = Colors.white;
+const Color textFieldFillColor = Color(0xFFF1F0F0);
