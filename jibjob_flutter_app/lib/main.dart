e6a9a792c65f2680719f8ea04a696d469e0394b3
LAGGOUNE Omar <dogie4bogie@gmail.com>
2025-05-17 02:02:56 +0100

    feat: Initialize Supabase client

--- jibjob_flutter_app/lib/main.dart
+++ jibjob_flutter_app/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'JibJobApp.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+      url: 'https://pyeonzdwjabqzlujisdo.supabase.co',
+      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZW9uemR3amFicXpsdWppc2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1OTEyNjgsImV4cCI6MjA2MTE2NzI2OH0.PT00SAxNBlzA4LRHoMyYFtHBytmCEm4CQPeWUTtUuqs',
+  );
+  
   runApp(JibJobApp());
 }
