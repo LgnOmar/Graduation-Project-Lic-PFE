f5170d9474488b58c0ecc136499c9e78d06a618f
LAGGOUNE Omar <dogie4bogie@gmail.com>
2025-05-19 05:44:25 +0100

    minor

--- jibjob_flutter_app/lib/main.dart
+++ jibjob_flutter_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:jibjob/services/supabase_service.dart';
 import 'JibJobApp.dart';
 import 'package:jibjob/screens/ClientSignUpPage.dart';
 
@@ -7,10 +9,12 @@ import 'package:jibjob/screens/ClientSignUpPage.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Supabase.initialize(
-      url: 'https://pyeonzdwjabqzlujisdo.supabase.co',
-      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZW9uemR3amFicXpsdWppc2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1OTEyNjgsImV4cCI6MjA2MTE2NzI2OH0.PT00SAxNBlzA4LRHoMyYFtHBytmCEm4CQPeWUTtUuqs',
-  );
+  await SupabaseService.initialize();
+
+  // await Supabase.initialize(
+  //     url: 'https://pyeonzdwjabqzlujisdo.supabase.co',
+  //     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZW9uemR3amFicXpsdWppc2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1OTEyNjgsImV4cCI6MjA2MTE2NzI2OH0.PT00SAxNBlzA4LRHoMyYFtHBytmCEm4CQPeWUTtUuqs',
+  // );
 
   runApp(
     MaterialApp( // Wrap your first page with MaterialApp
